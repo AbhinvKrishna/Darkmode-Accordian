03d716d7e27702f9760ef43032ff7d7d78cb0b12
Abhinav Krishna <123726072+AbhinvKrishna@users.noreply.github.com>
2023-02-03 06:54:43 -0800

    Updated app again

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import About from './Components/About';
+
+function App() {
+  return (
+    <div className='container my-3'>
+      <About/>
+
+    </div>
+  );
+}
+
+export default App;
